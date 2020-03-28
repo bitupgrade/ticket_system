da50b6a367afd9313cd05397f6ee49af7dfd0acb
Arnason <stefan.arnason@dxc.com>
2020-03-28 21:55:17 +0000

    added a priority field

--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,21 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+# Fields in ticket forum
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # FIX: Users tickets do not get deleted when 
     
+    TICKET_PRIORITY = [
+        ('Priority 4', 'Priority 4'),
+        ('Priority 3', 'Priority 3'),
+        ('Priority 2', 'Priority 2'),
+        ('Priority 1', 'Priority 1'),
+    ]
+    
+    ticket_priority = models.CharField(max_length=10, choices=TICKET_PRIORITY, default='Priority 4')
 
     def __str__(self):
         return self.title
